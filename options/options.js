81ede87d2c56acef5697de74f26c6f73e0705b08
Crowdbotics <team@crowdbotics.com>
2022-07-17 09:40:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "unikgospel_36234",
+  url: "https://unikgospel_36234.botics.co",
+  api: "https://unikgospel_36234.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
